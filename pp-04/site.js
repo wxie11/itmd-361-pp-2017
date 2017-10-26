fb8858ca29b1f641f4253e5e0661c8d08d604d86
Wenwen Xie <wxie11@hawk.iie.edu>
2017-10-25 21:05:09 -0500

    rewrite as self-executing anonymous function

--- pp-04/site.js
+++ pp-04/site.js
@@ -18,7 +18,17 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  namespace (e.g., neither its variables nor the double
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
+(function(){
+  var x = 5;
 
+  function double(num) {
+    x = num * 2;
+    return x;
+  }
+
+  double(2.5);
+  console.log('The value of x is:', x, 'It should be 5.');
+}) ();
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:

2587ee7324b2ea4e5df7ccef101884d1d082927d
Wenwen Xie <wxie11@hawk.iie.edu>
2017-11-09 21:47:23 -0600

    add event listener

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,9 +1,12 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
-
 $(document).ready(function(){
   $("#doorbell").remove();
 
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
+  ringbell.addEventListener("click", function() {
+    ringDoorbell();
+  });
 })
